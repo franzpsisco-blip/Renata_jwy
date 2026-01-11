53738d984118bb61a8436a799809d9c5939565b8
Franz Pablo Antezana Lopez <franzpsisco@gmail.com>
2026-01-11 12:25:53 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
   basePath: "/Renata_jwy",
   assetPrefix: "/Renata_jwy/",
+  env: {
+    NEXT_PUBLIC_BASE_PATH: "/Renata_jwy",
+  },
 };
 
 module.exports = nextConfig;
